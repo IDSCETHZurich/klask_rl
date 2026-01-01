7bd59acce3339d54baed511c78cd0867b8bab5f7
MeierTobias <meier.tobias@hotmail.com>
2026-01-01 23:03:30 +0100

    feat: isaac lab project created

--- docker_env/isaac-lab.sh
+++ docker_env/isaac-lab.sh
@@ -1,14 +1,25 @@
+CONTAINER_NAME="isaac-lab-local"
+
+# Get the directory of this script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 xhost +
-docker run --name isaac-lab --entrypoint bash -it --gpus all -e "ACCEPT_EULA=Y" --rm --network=host \
-   -e "PRIVACY_CONSENT=Y" \
-   -e DISPLAY \
-   -v $HOME/.Xauthority:/root/.Xauthority \
-   -v ~/docker/isaac-sim/cache/kit:/isaac-sim/kit/cache:rw \
-   -v ~/docker/isaac-sim/cache/ov:/root/.cache/ov:rw \
-   -v ~/docker/isaac-sim/cache/pip:/root/.cache/pip:rw \
-   -v ~/docker/isaac-sim/cache/glcache:/root/.cache/nvidia/GLCache:rw \
-   -v ~/docker/isaac-sim/cache/computecache:/root/.nv/ComputeCache:rw \
-   -v ~/docker/isaac-sim/logs:/root/.nvidia-omniverse/logs:rw \
-   -v ~/docker/isaac-sim/data:/root/.local/share/ov/data:rw \
-   -v ~/docker/isaac-sim/documents:/root/Documents:rw \
+docker run -it --rm \
+   --name="${CONTAINER_NAME}" \
+   --entrypoint bash \
+   --gpus=all \
+   --network=host \
+   --env="ACCEPT_EULA=Y" \
+   --env="PRIVACY_CONSENT=Y" \
+   --env="DISPLAY" \
+   --volume="$HOME/.Xauthority:/root/.Xauthority" \
+   --volume="${CONTAINER_NAME}_cache_kit:/isaac-sim/kit/cache:rw" \
+   --volume="${CONTAINER_NAME}_cache_ov:/root/.cache/ov:rw" \
+   --volume="${CONTAINER_NAME}_cache_pip:/root/.cache/pip:rw" \
+   --volume="${CONTAINER_NAME}_cache_glcache:/root/.cache/nvidia/GLCache:rw" \
+   --volume="${CONTAINER_NAME}_cache_computecache:/root/.nv/ComputeCache:rw" \
+   --volume="${CONTAINER_NAME}_logs:/root/.nvidia-omniverse/logs:rw" \
+   --volume="${CONTAINER_NAME}_data:/root/.local/share/ov/data:rw" \
+   --volume="${CONTAINER_NAME}_documents:/root/Documents:rw" \
+   --volume="$SCRIPT_DIR/../src:/workspace/klask_rl/src:rw" \
    nvcr.io/nvidia/isaac-lab:2.3.1
