d5b5eefc2954656cff57e3b476bdb83243586848
MeierTobias <meier.tobias@hotmail.com>
2026-02-11 22:12:54 +0100

    chore: new her compond config added

--- docker_env/ray.sh
+++ docker_env/ray.sh
@@ -15,6 +15,10 @@ echo "import ray; ray.init(); import time; [time.sleep(10) for _ in iter(int, 1)
 
 /workspace/isaaclab/_isaac_sim/python.sh -m pip install -e /workspace/klask_rl/source/klask_rl
 
+
 /workspace/isaaclab/_isaac_sim/python.sh /workspace/klask_rl/scripts/sb3/train_sac.py --config /workspace/klask_rl/scripts/sb3/config/experiments/klask_sac_two_stage_her.yaml
+
+docker exec -it -d isaac-lab-klask-rl_container_local /workspace/isaaclab/_isaac_sim/python.sh /workspace/klask_rl/scripts/sb3/train_sac.py --config /workspace/klask_rl/scripts/sb3/config/experiments/klask_sac_two_stage_her.yaml
+
 
 /workspace/isaaclab/_isaac_sim/python.sh /workspace/isaaclab/scripts/reinforcement_learning/ray/tuner.py --run_mode local --cfg_file /workspace/klask_rl/scripts/sb3/ray/hyperparameter_tuning/klask_sac_base_cfg.py --cfg_class KlaskSacHerTuneJobCfg --workflow /workspace/klask_rl/scripts/sb3/train_sac.py --metric rollout_ep_rew_mean --mode max --num_workers_per_node 2 --num_samples 2
