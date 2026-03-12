6bba4abadd330f52d567228b364f1bc2e41d99ca
MeierTobias <meier.tobias@hotmail.com>
2026-03-12 17:23:52 +0100

    feat: implement KlaskRlDreamerEnv to capture terminal observations before reset

--- src/klask_rl/source/klask_rl/klask_rl/tasks/manager_based/klask_rl/__init__.py
+++ src/klask_rl/source/klask_rl/klask_rl/tasks/manager_based/klask_rl/__init__.py
@@ -77,7 +77,7 @@ gym.register(
 
 gym.register(
     id="Klask-Rl-Dreamer-v0",
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    entry_point=f"{__name__}.klask_rl_dreamer_env:KlaskRlDreamerEnv",
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": f"{__name__}.klask_rl_env_cfg:KlaskRlDreamerEnvCfg",
